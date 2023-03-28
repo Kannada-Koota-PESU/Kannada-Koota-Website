f80f02ac293e7426e6e77df5535937650eecd50e
hitesh9929 <hiteshabishnoi@gmail.com>
2023-03-28 18:40:32 +0530

    okk

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 
 function Landing() {
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+    {/* <Route path='/' element={<Loader/>}></Route> */}
+    {/* <Route path='/landing' element={<Landing/>}></Route> */}
+    
+    </Routes>
+    </BrowserRouter>
 
   <p>lessgo!!</p>
 </>
